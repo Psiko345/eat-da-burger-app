9c0d08e3bf0214e078219a3432d714dbb68a6f96
Joshua Piper <joshua.g.piper@gmail.com>
2020-08-15 12:38:53 +1000

    created put route to update devoured boolean value and update db.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -26,3 +26,24 @@ router.post("/api/burgers", (req, res) => {
     }
   );
 });
+
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
