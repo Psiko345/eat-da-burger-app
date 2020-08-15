36226e2964bb52bef84e9fa9a665249f7e1509af
Joshua Piper <joshua.g.piper@gmail.com>
2020-08-15 12:31:16 +1000

    created post route

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+const burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+  burger.all((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+  burger.create(
+    ["name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    (result) => {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    }
+  );
+});
