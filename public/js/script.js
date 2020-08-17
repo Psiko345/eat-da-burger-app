97ce217fff1512b3043d6228717b64669c224e7a
Joshua Piper <joshua.g.piper@gmail.com>
2020-08-17 11:48:11 +1000

    expanded handlebars and css files, added burger icon, updated server file

---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+$(() => {
+  $(".change-devoured").on("click", function (event) {
+    const id = $(this).data("id");
+    const newDevoured = $(this).data("newdevour");
+
+    const newDevouredState = {
+      devoured: newDevoured,
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState,
+    }).then(() => {
+      console.log("changed devoured to", newDevoured);
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", (event) => {
+    event.preventDefault();
+
+    const newBurger = {
+      name: $("#burger").val().trim(),
+      devoured: $("input[name=devoured]:checked").val().trim(),
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(() => {
+      console.log("created new burger");
+      location.reload();
+    });
+  });
+});
